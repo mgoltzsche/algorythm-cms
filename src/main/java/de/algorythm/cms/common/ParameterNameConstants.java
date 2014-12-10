19bee2be4d7740e68a633583b5a6dc06f007c93d
max <max.goltzsche@gmail.com>
2014-12-10 02:17:58 +0100

    Using nio filesystems. Localization. More stable clientside ajaxuri transformation

--- src/main/java/de/algorythm/cms/common/ParameterNameConstants.java
+++ src/main/java/de/algorythm/cms/common/ParameterNameConstants.java
@@ -6,8 +6,8 @@ public class ParameterNameConstants {
 		
 		static public final String SITE_PARAM_PREFIX =  "site.param.";
 		static public final String SITE_NAME =          "site.name";
-		static public final String RELATIVE_BASE_URL =  "relativeBaseUrl";
-		static public final String RESOURCE_DIRECTORY = "resourceDirectory";
+		static public final String RELATIVE_BASE_URL =  "relativeBaseURL";
+		static public final String RESOURCE_BASE_URL = "resourceBaseURL";
 		static public final String PAGE_PARAM_PREFIX =  "page.param.";
 		static public final String PAGE_PATH =          "page.path";
 		static public final String PAGE_TITLE =         "page.title";
